3f582b1dcb7c7e937359ebb3017b593708f81b02
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2023-03-15 07:31:18 +0700

    Update home.php

--- php/home.php
+++ php/home.php
@@ -5,6 +5,26 @@
 
 
 <div class="bg-white py-12 sm:py-12">
+	<?php if (pluginActivated('pluginSearch')): ?>
+<div class="form-inline d-block">
+<input id="search-input" class="form-control mr-sm-2" type="search" placeholder="<?php $language->p('Search') ?>" aria-label="Search">
+<button class="btn btn-outline-dark my-2 my-sm-0" type="button" onClick="searchNow()"><?php $language->p('Search') ?></button>
+<script>
+function searchNow() {
+var searchURL = "<?php echo Theme::siteUrl(); ?>search/";
+window.open(searchURL + document.getElementById("search-input").value, "_self");
+}
+document.getElementById("search-input").onkeypress = function(e) {
+if (!e) e = window.event;
+var keyCode = e.keyCode || e.which;
+if (keyCode == '13') {
+searchNow();
+return false;
+}
+}
+</script>
+</div>
+<?php endif ?>
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="mx-auto max-w-2xl lg:mx-0">
       <h3 class="text-3xl font-bold tracking-tight text-indigo-600 sm:text-4xl">Update News</h3>
